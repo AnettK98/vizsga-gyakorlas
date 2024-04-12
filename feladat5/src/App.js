cb4310f668eca0a81687a69e863f54eac5691c97
Anett <katonaanett1123@gmail.com>
2024-04-12 17:06:39 +0200

    feladat5

--- feladat5/src/App.js
+++ feladat5/src/App.js
@@ -1,6 +1,24 @@
 import 'bootstrap/scss/bootstrap.scss';
+import { useState } from "react";
+import Konyv from './Konyv';
 
 function App() {
+  const [title, setTitle]= useState('')
+  const [isbn, setIsbn]= useState('')
+  const [message, setMessage]= useState('')
+
+  function handleClick() {
+    try{
+      const k = new Konyv(title, isbn)
+      setMessage('')
+      setTitle('')
+      setIsbn('')
+      console.log(k.toString())
+    }
+    catch (error) {
+      setMessage(error.message)
+    }
+  }
 
   return (
     <div className='container'>
@@ -10,6 +28,8 @@ function App() {
           type='text'
           className='form-control'
           id='title'
+          value={title}
+          onChange={e => setTitle(e.target.value)}
         />
       </div>
       <div className='mb-2'>
@@ -18,14 +38,17 @@ function App() {
           type='text'
           className='form-control'
           id='isbn'
+          value={isbn}
+          onChange={e => setIsbn(e.target.value)}
         />
       </div>
-
+      {message && <div className='alert alert-danger'>{message}</div>}
       <div className='mb-2'>
         <input
           type='button'
           className='btn btn-primary'
           value='Új könyv'
+          onClick={handleClick}
         />
       </div>
     </div>
