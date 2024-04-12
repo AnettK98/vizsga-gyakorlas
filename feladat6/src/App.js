b15573e5254f94619910ee58fc6824452d97b91e
Anett <katonaanett1123@gmail.com>
2024-04-12 17:38:29 +0200

    6. feladat kész

--- feladat6/src/App.js
+++ feladat6/src/App.js
@@ -1,8 +1,42 @@
 import 'bootstrap/scss/bootstrap.scss'
 import './App.scss'
+import { useEffect, useState } from 'react'
+import loading from './feladat6-loading.gif'
 
 function App() {
+    const [konyvek, setKonyvek] = useState(null)
 
+    useEffect(() => {
+        fetch('http://localhost:4000/konyvek')
+            .then(data => data.json())
+            .then(json => setKonyvek(json))
+            .catch(console.log)
+    }, [])
+
+if (!konyvek)
+    return <img src={loading} alt='Betöltés'/>
+    
+    return(
+        <div className="container">
+            <table className='table table-striped'>
+                <thead>
+                    <tr>
+                        <th>Cím</th>
+                        <th>Polc</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {konyvek.map(konyv =>(
+                        <tr key={konyv.id}>
+                            <td>{konyv.cim}</td>
+                            <td>{konyv.polc}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
+
 
 export default App
